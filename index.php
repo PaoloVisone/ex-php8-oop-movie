633efd53bf1cfbc602a61b88539880bff89593e9
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-05 21:19:57 +0200

    Add Movie class with properties and constructor

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <?php
 
-class Movie {}
+class Movie
+{
+    public $nome;
+    public $regista;
+    public $durata;
+    public $data;
+
+    function __construct($nome, $regista, $durata, $data)
+    {
+        $this->nome = $nome;
+        $this->regista = $regista;
+        $this->durata = $durata;
+        $this->data = $data;
+    }
+}
 
 
 // Instanze
 
-$matrix = new Movie();
-$titanic = new Movie();
-$inception = new Movie();
+$matrix = new Movie("Matrix", " Lana Wachowski, Lilly Wachowski", "2h 16m", "7 maggio 1999");
+$titanic = new Movie("Titanic", "James Cameron", "3h 14m", "16 gennaio 1998");
+$inception = new Movie("Inception", "Christopher Nolan", "2h 28m", "24 settembre 2010");
+
 
 ?>
 
