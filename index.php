cd1978073596b34550f8d1a6856f96884b5c027f
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-05 21:39:28 +0200

    Add isOld method to Movie class and update instance creation

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ class Movie
     public $durata;
     public $data;
 
+    function isOld()
+    {
+        if ($this->data < 2000) {
+            return "Il film '{$this->nome}' diretto da {$this->regista}, della durata di {$this->durata}, è uscito prima del 2000";
+        }
+        return null;
+    }
+
     function __construct($nome, $regista, $durata, $data)
     {
         $this->nome = $nome;
@@ -19,10 +27,14 @@ class Movie
 
 // Instanze
 
-$matrix = new Movie("Matrix", " Lana Wachowski, Lilly Wachowski", "2h 16m", "7 maggio 1999");
-$titanic = new Movie("Titanic", "James Cameron", "3h 14m", "16 gennaio 1998");
-$inception = new Movie("Inception", "Christopher Nolan", "2h 28m", "24 settembre 2010");
+$matrix = new Movie("Matrix", " Lana Wachowski, Lilly Wachowski", "2h 16m", 1999);
+$titanic = new Movie("Titanic", "James Cameron", "3h 14m", 1998);
+$inception = new Movie("Inception", "Christopher Nolan", "2h 28m", 2010);
 
+var_dump($matrix);
+var_dump($matrix->isOld());
+var_dump($titanic->isOld());
+var_dump($inception->isOld());
 
 ?>
 
