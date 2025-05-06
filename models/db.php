7617b811cb05065a9c15e082cc2c1b67dfa6b705
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-06 19:59:14 +0200

    Add db.php file to instantiate Movie objects and set their ratings

---
+++ models/db.php
@@ -0,0 +1,21 @@
+<?php
+
+// Istanza dei film
+$matrix = new Movie("Matrix", "Lana Wachowski, Lilly Wachowski", "2h 16m", 1999, [$sciFi, $action]);
+$titanic = new Movie("Titanic", "James Cameron", "3h 14m", 1998, [$romance, $drama]);
+$inception = new Movie("Inception", "Christopher Nolan", "2h 28m", 2010, [$action, $sciFi]);
+
+// Rating per i film
+$matrix->setRating(9);
+$titanic->setRating(8.5);
+$inception->setRating(9.2);
+
+// var_dump($matrix);
+// var_dump($titanic);
+// var_dump($inception);
+// var_dump($matrix->isOld());
+// var_dump($titanic->isOld());
+// var_dump($inception->isOld());
+
+// Array dei film
+$movies = [$matrix, $titanic, $inception];
